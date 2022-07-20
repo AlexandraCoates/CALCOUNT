37060f8d0ecebe9c38c1fcdd0b34b462c344112a
Alexandra Coates <72305229+AlexandraCoates@users.noreply.github.com>
2022-07-20 18:30:08 +0100

    Making the nav bar work

--- app/src/main/java/com/example/calcount3/MainActivity.java
+++ app/src/main/java/com/example/calcount3/MainActivity.java
@@ -5,40 +5,118 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView navigationView;
+    public Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
+        navigationView = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(navigationView);
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item)
+                    {
+                        selectDrawerItem(item);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    public void selectDrawerItem(MenuItem item)
+    {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(item.getItemId())
+        {
+            case R.id.nav_add_edit_records:
+                fragmentClass = Add_Edit_Records.class;
+                break;
+            case R.id.nav_monthly:
+                fragmentClass = Monthly_Records.class;
+                break;
+            case R.id.nav_weekly:
+                fragmentClass = Weekly_Records.class;
+                break;
+            case R.id.edit_Details:
+                fragmentClass = Edit_Details.class;
+                break;
+            case R.id.yourTrends:
+                fragmentClass = YourTrends.class;
+                break;
+            default:
+                fragmentClass = Add_Edit_Records.class;
+                break;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
+
+        drawerLayout.closeDrawers();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+
+
+
+
 }
